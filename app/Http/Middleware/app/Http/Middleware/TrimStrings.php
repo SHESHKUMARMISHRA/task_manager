d3f71ecad07a8a92ee8169f36491a938718898f3
SHESHKUMARMISHRA <mishrakshesh14287@gmail.com>
2025-01-08 17:05:02 +0530

    added new middleware missign files

---
+++ app/Http/Middleware/app/Http/Middleware/TrimStrings.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware\app\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Foundation\Http\Middleware\TrimStrings as Middleware;
+
+class TrimStrings extends Middleware
+{
+    /**
+     * The names of the attributes that should not be trimmed.
+     *
+     * @var array
+     */
+    protected $except = [
+        'password',
+        'password_confirmation',
+    ];
+   
+}
